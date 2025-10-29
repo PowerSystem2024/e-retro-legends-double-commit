349147c82a483970c75994a2c3a77940cb53c782
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-29 17:12:23 -0300

    formatted code

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,19 +1,22 @@
 import cors from "cors";
 
-const ALLOWED_ORIGINS = ["http://localhost:5000", "https://e-retro-legends.vercel.app"]
+const ALLOWED_ORIGINS = [
+  "http://localhost:5000",
+  "https://e-retro-legends.vercel.app",
+];
 
 export const corsMiddleware = ({ allowedOrigins = ALLOWED_ORIGINS } = {}) => {
-    cors({
-        origin: (origin, callback) => {
-            if (allowedOrigins.includes(origin)) {
-                return callback(null, true)
-            }
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
 
-            if (!origin) {
-                return callback(null, true)
-            }
+      if (!origin) {
+        return callback(null, true);
+      }
 
-            return callback(new Error("Not allowed by CORS"))
-        }
-    })
-}
+      return callback(new Error("Not allowed by CORS"));
+    },
+  });
+};

2f45a7abb15cfccb360836131fea58adad31be82
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-11-06 02:00:01 -0300

    updated filter

--- frontend/src/pages/Public/Products.jsx
+++ frontend/src/pages/Public/Products.jsx
@@ -44,6 +44,7 @@ export const AllProducts = () => {
       })
     : products;
 
+    console.log({ products:normalize( products.map((p) => p.category)) })
   return (
     <div className="min-h-screen bg-gray-50">
       <section className="max-w-7xl mx-auto px-4 py-10">

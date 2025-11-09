b58872312ed0b487b181ff64e7ae8db4faa9e5cc
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-11-09 17:44:23 -0300

    updated url payment

--- frontend/src/contexts/CartContext.jsx
+++ frontend/src/contexts/CartContext.jsx
@@ -176,7 +176,7 @@ export const CartProvider = ({ children }) => {
     setLoadingPayment(true);
     try {
       const response = await fetch(
-        `http://localhost:5000/api/payment/create`,
+        `${import.meta.env.VITE_BACK_API_URL}/api/payment/create`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
